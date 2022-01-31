758059edae65c6f2744b2c0bfa9895f033b47b70
Santiago Correa <santiago@dnlab.de>
2022-01-31 21:10:11 +0100

    Add slider

--- components/front/Gallery.tsx
+++ components/front/Gallery.tsx
@@ -1,26 +1,55 @@
 import React, { useContext, useEffect } from 'react';
 import styled from 'styled-components';
 import ContainerComponent from './Container';
+import Slider from 'react-slick';
 
 interface ComponentProps {}
 
 const GalleryComponent: React.FC<ComponentProps> = () => {
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    lazyLoad: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <ContainerComponent>
       <Gallery>
         <GalleryTitle>Fotogalerie</GalleryTitle>
         <GalleryInner>
           <GalleryImageList>
-            <GalleryPanel>
+            <Slider {...settings}>
               <GalleryImage alt="" src="/images/data/gal1.jpg" />
               <GalleryImage alt="" src="/images/data/gal2.jpg" />
               <GalleryImage alt="" src="/images/data/gal3.jpg" />
-            </GalleryPanel>
-            <GalleryPanel>
+
               <GalleryImage alt="" src="/images/data/gal4.jpg" />
               <GalleryImage alt="" src="/images/data/gal5.jpg" />
               <GalleryImage alt="" src="/images/data/gal6.jpg" />
-            </GalleryPanel>
+            </Slider>
           </GalleryImageList>
         </GalleryInner>
       </Gallery>
@@ -28,82 +57,29 @@ const GalleryComponent: React.FC<ComponentProps> = () => {
   );
 };
 
-const GalleryPanel = styled.p`
-  margin-bottom: 0;
-  position: relative;
-  width: 100%;
-  display: inline-block;
-  height: 595px;
-
-  @media screen and (max-width: 768px) {
-    height: 100%;
-  }
-
-  img {
-    position: absolute;
-
-    @media screen and (max-width: 768px) {
-      position: static;
-    }
-  }
-`;
-
 const GalleryImageList = styled.div`
   border-radius: 20px;
-  display: flex;
-  justify-content: space-between;
+
   margin-right: 1px;
   overflow: hidden;
-  width: 100%;
   margin: 0 auto;
 
-  @media screen and (max-width: 768px) {
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
+  .slick-slider {
+    object-fit: contain;
+
+    margin: 0 auto;
     text-align: center;
-  }
+    height: 400px;
 
-  img {
-    @media screen and (max-width: 768px) {
-      width: 33%;
-      height: 275px;
-      object-fit: cover;
+    .slick-slide {
+      padding-right: 10px;
     }
 
-    @media screen and (max-width: 500px) {
-      width: 275px;
+    img {
+      height: 400px;
+      max-height: 100%;
+      object-fit: contain;
     }
-  }
-
-  p:nth-of-type(1) img:nth-of-type(1) {
-    left: 0;
-    top: 0;
-  }
-
-  p:nth-of-type(1) img:nth-of-type(2) {
-    bottom: 0;
-    left: 0;
-  }
-
-  p:nth-of-type(1) img:nth-of-type(3) {
-    right: 5px;
-    top: 0;
-  }
-
-  p:nth-of-type(2) img:nth-of-type(1) {
-    left: -5px;
-    top: 0;
-  }
-
-  p:nth-of-type(2) img:nth-of-type(2) {
-    right: 10px;
-    top: 0;
-  }
-
-  p:nth-of-type(2) img:nth-of-type(3) {
-    bottom: 0;
-    right: 10px;
   }
 `;
 
