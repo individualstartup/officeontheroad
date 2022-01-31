0fd28f375408616a7a61b111996d34b360545f18
Santiago Correa <santiago@dnlab.de>
2022-01-31 21:11:31 +0100

    Only show 2 slides instead of 3

--- components/front/Gallery.tsx
+++ components/front/Gallery.tsx
@@ -10,7 +10,7 @@ const GalleryComponent: React.FC<ComponentProps> = () => {
     dots: true,
     infinite: false,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 2,
     slidesToScroll: 1,
     initialSlide: 0,
     lazyLoad: true,
@@ -18,8 +18,8 @@ const GalleryComponent: React.FC<ComponentProps> = () => {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 1,
           infinite: true,
           dots: true,
         },
@@ -71,9 +71,7 @@ const GalleryImageList = styled.div`
     text-align: center;
     height: 400px;
 
-    .slick-slide {
-      padding-right: 10px;
-    }
+
 
     img {
       height: 400px;
