f1e900a3cf5620eecc1c3fcff9c90aa3f1d6cd1f
Miloslav Vlach <miloslav.vlach@gmail.com>
2023-05-12 21:39:39 +0200

    wrong endpoint

--- api/apiClient.ts
+++ api/apiClient.ts
@@ -3,8 +3,8 @@ import { Configuration } from './runtime';
 
 const CONFIGURATION = new Configuration({
   /*headers: { Authorization: 'Hash a5d37ad5-e231-43cc-be33-21721f159b91' },*/
-  basePath: 'http://localhost:8080/services/frontend-service',
-  /*basePath: 'https://www.caravano.cz/services/frontend-service',*/
+  /*basePath: 'http://localhost:8080/services/frontend-service',*/
+  basePath: 'https://www.caravano.cz/services/frontend-service',
 });
 
 export const apiClient = new PublicApiApi(CONFIGURATION);
